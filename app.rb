5ba119e1b0302d50426d274cc4ef7793c9fd2971
mggbhn <maggiebswim@gmail.com>
2018-07-17 15:37:18 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,7 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
   get '/' do
   "Hello, World!"
+  end
+  
+  get '/name' do
+  "My name is Megan and Maggie."
+  end
+  
+  get '/hometown' do
+  "My hometown is Mansfield and Biddeford."
+  end
+  
+  get '/favorite-song' do
+  "My favorite song is Happy Birthday!"
   end
 end
